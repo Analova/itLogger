cb1e2a79f0ed4be2769d0d327c63395bc18e0493
Ana Nikolova <ani.nik19@gmail.com>
2019-08-22 08:11:45 +0200

    search a log

--- it/src/actions/logActions.js
+++ it/src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from "./types";
 
 // Get logs from server
@@ -19,6 +20,26 @@ export const getLogs = () => async dispatch => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
+
+// Search logs from server
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data
     });
   } catch (err) {
